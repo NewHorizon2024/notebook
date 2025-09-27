486fd382e06bef36cbc98e19b3f14fa7dce52d9b
mostafa <mustafa.husseiny@gmail.com>
2025-09-27 22:17:54 +0200

    adding suspense

--- app/bucket/page.tsx
+++ app/bucket/page.tsx
@@ -1,15 +1,18 @@
+import { Suspense } from "react";
 import CommandCreator from "../_components/commands/command-creator/CommandCreator";
 import CommandsList from "../_components/commands/commands-list/CommandsList";
 
 export default async function Page() {
   return (
-    <div className="grid grid-cols-12">
-      <div className="col-span-12 sm:col-span-6 md:col-span-6 lg:col-span-6 xl:col-span-4">
-        <CommandCreator />
+    <Suspense>
+      <div className="grid grid-cols-12">
+        <div className="col-span-12 sm:col-span-6 md:col-span-6 lg:col-span-6 xl:col-span-4">
+          <CommandCreator />
+        </div>
+        <div className="col-span-12 sm:col-span-6 md:col-span-6 lg:col-span-6 xl:col-span-8 ">
+          <CommandsList />
+        </div>
       </div>
-      <div className="col-span-12 sm:col-span-6 md:col-span-6 lg:col-span-6 xl:col-span-8 ">
-        <CommandsList />
-      </div>
-    </div>
+    </Suspense>
   );
 }
