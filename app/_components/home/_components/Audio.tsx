c0ab3221b3c10becc791a86b0388b58ee8dfa1e3
mostafa <mustafa.husseiny@gmail.com>
2025-10-05 12:57:43 +0200

    show audio

--- app/_components/home/_components/Audio.tsx
+++ app/_components/home/_components/Audio.tsx
@@ -5,7 +5,7 @@ export default function AudioPlayer({ fileSrc }: AudioProps) {
   return (
     <figure>
       <figcaption hidden>Listen to the T-Rex:</figcaption>
-      <audio hidden autoPlay controls src={fileSrc}></audio>
+      <audio  autoPlay controls src={fileSrc}></audio>
     </figure>
   );
 }
